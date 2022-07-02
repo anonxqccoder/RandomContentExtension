e30e73a288584e8836225f3157bd87e329780539
anonxqccoder <108593255+anonxqccoder@users.noreply.github.com>
2022-07-02 17:15:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+**For xQc (https://www.twitch.tv/videos/1516937671?t=1h46m25s)**
+
+To xQc, if you actually want to pay me for it, which would be dope but not required, dm me on twitter: https://twitter.com/877445643216df
+
+If there's something you would like to change about it, you can dm me for that as well :), or have one of the Juicer's make a github issue (someone in chat will know)
+
+**How to use:**
+* **Installation:**
+    1. Download latest release zip file: https://github.com/anonxqccoder/randomcontentbutton/releases
+    2. Extract the zip somewhere
+    2. Go to the extensions page of your Chrome Browser or type chrome://extensions/ in your url bar and hit enter
+    3. Enable developer mode
+    4. Click load unpacked, browse to the extracted folder, then click select Folder
+* **Use Extension:**
+    * For optimal usage, it is best to pin the extension
+    * Click the extension logo
+        * **The "CONTENT" button will open a random link from your submitted links**
+        * **The upload links button that says "Choose File" will let you submit links**
+            * Make a text file, the name doesn't matter
+            * Separate links line by line, you can see the example.txt file on the github page if you're confused
+            * Click "Choose File" and upload the file like normally, the links will upload to the extension automatically
+        * **The "NUKE ALL LINKS (IRREVERISBLE)" button will remove all of your submitted links from the extension, you cannot go back once you confirm the nuke**
+        * **Under "Current links:" will be your links**
+            * There is the link itself
+            * A checkbox to the right of the link, if you wish to disable the link without removing it
+            * A remove button, if you wish to remove the link
+
+It may look like a lot, but its actually quite simple. Here is a video of the extension being installed and used:
+
+URL HERE
---
+++ src/script.js
@@ -0,0 +1,106 @@
+const asyncSet = (links) => new Promise(resolve => {
+    chrome.storage.local.set({ links: links }, function () {
+        resolve()
+    });
+})
+
+const asyncGet = () => new Promise(resolve => {
+    chrome.storage.local.get({ links: [] }, function (result) {
+        resolve(result.links)
+    });
+})
+
+const linksButton = document.getElementById("uploadLinksButton")
+linksButton.addEventListener("change", (e) => {
+    const read = new FileReader();
+
+    read.readAsBinaryString(e.target.files[0]);
+
+    read.onloadend = async function () {
+        const currentLinks = await asyncGet()
+        const newLinks = read.result.split("\n").map(link => link.trim()).filter(link => !currentLinks.find(linkData => linkData.url === link))
+        const validLinks = currentLinks.concat(newLinks.map(link => ({ url: link, enabled: true })))
+        await asyncSet(validLinks)
+        reloadLinks()
+        e.target.value = null
+    }
+})
+
+async function reloadLinks() {
+    const validLinks = await asyncGet()
+    const linksCollectionDiv = document.getElementById("links")
+    linksCollectionDiv.innerHTML = ""
+    for (const linkData of validLinks) {
+        const linkParagraphElement = document.createElement("p")
+        const linkHref = document.createElement("a")
+        linkHref.href = linkData.url
+        linkHref.innerText = linkData.url
+        linkParagraphElement.appendChild(linkHref)
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.checked = linkData.enabled
+        checkbox.onchange = () => checkboxChange(!linkData.enabled, linkData.url)
+        linkParagraphElement.appendChild(checkbox)
+        const removeButton = document.createElement('button')
+        removeButton.innerText = "Remove"
+        removeButton.onclick = () => removeLink(linkData.url)
+        linkParagraphElement.appendChild(removeButton)
+        console.log(linkParagraphElement)
+        linksCollectionDiv.appendChild(linkParagraphElement)
+    }
+}
+
+async function checkboxChange(isChecked, url) {
+    const links = await asyncGet()
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].url === url) {
+            links[i].enabled = isChecked
+            break;
+        }
+    }
+    await asyncSet(links)
+    reloadLinks()
+}
+
+async function removeLink(url) {
+    const links = await asyncGet()
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].url === url) {
+            links.splice(i, 1)
+            break;
+        }
+    }
+    await asyncSet(links)
+    reloadLinks()
+}
+
+const contentButton = document.getElementById("contentButton")
+contentButton.addEventListener("click", async () => {
+    const links = (await asyncGet()).filter(linkData => linkData.enabled)
+    if (links.length === 0) {
+        return alert("No enabled links in database!")
+    }
+    chrome.tabs.create({
+        url: links[Math.floor(Math.random() * links.length)].url,
+        selected: true,
+    })
+})
+
+const nukeConfirm = {confirmed: false, time: Date.now()}
+document.getElementById("nuke").addEventListener("click", async () => {
+    if (Date.now()-nukeConfirm.time > 20000) {
+        nukeConfirm.confirmed = false;
+    }
+    if (!nukeConfirm.confirmed) {
+        alert("This action is IRREVERSIBLE, please click the button one more time within 20 seconds to confirm!")
+        nukeConfirm.confirmed = true
+        nukeConfirm.time = Date.now()
+    } else {
+        nukeConfirm.confirmed = false;
+        nukeConfirm.time = Date.now()
+        await asyncSet([])
+        reloadLinks()
+    }
+})
+
+reloadLinks()
